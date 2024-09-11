a7233a2055729f84e5a7bf4b7bffb5d1bc3738ab
Luigi Iossa <123959081+Luigi-stack@users.noreply.github.com>
2024-09-11 17:57:05 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,7 +38,7 @@ function Contact() {
                 <Row className="mb-5">
                     <Col sm={12} md={4} className="d-flex flex-column align-items-center">
                         <h3>Address</h3>
-                        <Link to="#" className="text-secondary fs-5 link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer">Bruxelles</Link>
+                        <Link to="" className="text-secondary fs-5 link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer">Bruxelles</Link>
                     </Col>
 
                     <Col sm={12} md={4} className="d-flex flex-column align-items-center">
